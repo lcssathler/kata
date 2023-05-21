74dece4016b96de62524630d225162f7707b2200
Lucas Sathler <lcssathler@gmail.com>
2023-05-21 17:21:55 -0300

    kata 15

---
+++ src/kata/pratice/aaoConvertNumberToString.java
@@ -0,0 +1,12 @@
+package pratice;
+
+public class aaoConvertNumberToString {
+    public static String numberToString(int num) {
+        return String.valueOf(num);
+    }
+
+    public static void main(String[] args) {
+        final String s = numberToString(5);
+        System.out.println(s);
+    }
+}
