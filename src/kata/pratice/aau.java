21de3cd6d99368d9e34a23fbbc9b16e82c0af5dc
Lucas Sathler <lcssathler@gmail.com>
2023-05-27 10:15:58 -0300

    kata 21

---
+++ src/kata/pratice/aau.java
@@ -0,0 +1,13 @@
+package pratice;
+
+import java.math.MathContext;
+
+public class aau {
+    public static String even_or_odd(int number) {
+        if (number % 2 == 0) {
+            return "even";
+        } else {
+            return "odd";
+        }
+    }
+}
