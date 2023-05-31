8e79b8600c8876928fddbf5807e237ea2c112257
Lucas Sathler <lcssathler@gmail.com>
2023-05-31 18:53:03 -0300

    kata 26 - A Needle in the Haystack

---
+++ src/kata/pratice/aazNeedleInTheHaystack.java
@@ -0,0 +1,26 @@
+package pratice;
+import java.util.Arrays;
+import java.util.List;
+
+public class aazNeedleInTheHaystack {
+    public static String findNeedle(Object[] haystack) {
+        if (haystack == null) {
+            return "Invalid object!";
+        }
+        List<Object> objects = Arrays.asList(haystack);
+        int needleIndex = objects.indexOf("needle");
+        return "found the needle at position " + needleIndex;
+    }
+
+    public static void main(String[] args) {
+        Object[] haystack1 = {"3", "123124234", null, "needle", "world", "hay", 2, "3", true, false};
+        Object[] haystack2 = {"283497238987234", "a dog", "a cat", "some random junk", "a piece of hay", "needle", "something somebody lost a while ago"};
+        Object[] haystack3 = {1,2,3,4,5,6,7,8,8,7,5,4,3,4,5,6,67,5,5,3,3,4,2,34,234,23,4,234,324,324,"needle",1,2,3,4,5,5,6,5,4,32,3,45,54};
+        Object[] haystack4 = null;
+
+        System.out.println(findNeedle(haystack1));
+        System.out.println(findNeedle(haystack2));
+        System.out.println(findNeedle(haystack3));
+        System.out.println(findNeedle(haystack4));
+    }
+}
