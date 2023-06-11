6686e4d5f1973de360210084f4dd26f7269b4c0e
Lucas Sathler <lcssathler@gmail.com>
2023-06-11 18:13:11 -0300

    kata 33 - Convert a string to an array

---
+++ src/kata/pratice/abhStringToArray.java
@@ -0,0 +1,12 @@
+package pratice;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class abhStringToArray {
+    public static String[] stringToArray(String s) {
+        String[] split = s.split(" ");
+        return split;
+    }
+}
