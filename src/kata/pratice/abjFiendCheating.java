950d29403684c95adcd3e3f11cd6661887776cd6
Lucas Sathler <lcssathler@gmail.com>
2023-06-13 21:02:55 -0300

    kata 35 - Is my friend cheating?

---
+++ src/kata/pratice/abjFiendCheating.java
@@ -0,0 +1,36 @@
+package pratice;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class abjFiendCheating {
+    public static List<long[]> removNb(long n) {
+        List<long[]> results = new ArrayList<>();
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < n; j++) {
+                if (i * j == sumSequence(n) - i - j) {
+                    results.add(new long[]{i, j});
+                }
+            }
+        }
+        return results;
+    }
+
+    public static long sumSequence(long n) {
+        long sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        List<long[]> longs = removNb(26);
+        for (long[] aLong : longs) {
+            for (long l : aLong) {
+                System.out.println(l);
+            }
+        }
+    }
+}
